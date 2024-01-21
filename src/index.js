b0063bc2a4a30e7381826b119a2132b779b26e84
Suhaib Ahmed Shaikh <suhaibshakilshaikh@gmail.com>
2024-01-21 21:03:50 +0530

    utils updated

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ dotenv.config({
     path: './env'
 })
 
-connectDB()
+
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+}).catch((err) => {
+    console.log("MONGO DB Connection failed!!! ", err);
+})
